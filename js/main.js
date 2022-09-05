399dff919476ff1ca45ce3c6ff6c05334e1da3c8
Steve Fallet <steve.fallet@gmail.com>
2022-09-05 16:24:14 +0200

    1st

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Conversion de degrés Celsius en Farenheit
+ * @author  Steve Fallet
+ * @version 0.1
+ * @since   2022-09-05
+ */
+
+(function main() {
+  'use strict';
+
+  // Ton code de malade ici ...
+
+}()); // Main IIFE
